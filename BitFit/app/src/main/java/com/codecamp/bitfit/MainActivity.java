561d171046ab3ae9db8184c21ff6e225380451d6
Maximilian Breida <maxbreida@gmail.com>
2018-02-12 13:58:01 +0100

    add simple bottom navigation

--- BitFit/app/src/main/java/com/codecamp/bitfit/MainActivity.java
+++ BitFit/app/src/main/java/com/codecamp/bitfit/MainActivity.java
@@ -3,11 +3,43 @@ package com.codecamp.bitfit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+
+import net.steamcrafted.materialiconlib.MaterialDrawableBuilder;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initBottomNavigation();
+    }
+
+    private void initBottomNavigation() {
+        AHBottomNavigation bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        // create items
+        AHBottomNavigationItem home = new AHBottomNavigationItem(getString(R.string.home_tab),
+                MaterialDrawableBuilder.with(this).setIcon(MaterialDrawableBuilder.IconValue.HOME).build());
+        AHBottomNavigationItem pushUps = new AHBottomNavigationItem(getString(R.string.push_up_tab),
+                MaterialDrawableBuilder.with(this).setIcon(MaterialDrawableBuilder.IconValue.LIGHTBULB).build());
+        AHBottomNavigationItem squats = new AHBottomNavigationItem(getString(R.string.squat_tab),
+                MaterialDrawableBuilder.with(this).setIcon(MaterialDrawableBuilder.IconValue.BABY).build());
+        AHBottomNavigationItem run = new AHBottomNavigationItem(getString(R.string.run_tab),
+                MaterialDrawableBuilder.with(this).setIcon(MaterialDrawableBuilder.IconValue.RUN).build());
+        AHBottomNavigationItem profile = new AHBottomNavigationItem(getString(R.string.profile_tab),
+                MaterialDrawableBuilder.with(this).setIcon(MaterialDrawableBuilder.IconValue.FACE_PROFILE).build());
+
+        bottomNavigation.addItem(home);
+        bottomNavigation.addItem(pushUps);
+        bottomNavigation.addItem(squats);
+        bottomNavigation.addItem(run);
+        bottomNavigation.addItem(profile);
+
+        bottomNavigation.setCurrentItem(0);
+
     }
 }
