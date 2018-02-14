e170b5d1e112170b404620a18865b92828b0fd27
Maximilian Breida <maxbreida@gmail.com>
2018-02-14 11:03:47 +0100

    worked on pushup highscores homescreen, fixed bug on entering name, included util class to reduce code repitition

--- app/src/main/java/com/codecamp/bitfit/statistics/RunStatisticsActivity.java
+++ app/src/main/java/com/codecamp/bitfit/statistics/RunStatisticsActivity.java
@@ -3,6 +3,8 @@ package com.codecamp.bitfit.statistics;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.codecamp.bitfit.R;
+
 public class RunStatisticsActivity extends AppCompatActivity {
 
     @Override
