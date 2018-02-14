e9882b34c3a9845609f9982e85ed6f1219da66e5
Maximilian Breida <maxbreida@gmail.com>
2018-02-14 09:49:30 +0100

    add activities for statistics

---
+++ app/src/main/java/com/codecamp/bitfit/statistics/SquatStatisticsActivity.java
@@ -0,0 +1,15 @@
+package com.codecamp.bitfit.statistics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.codecamp.bitfit.R;
+
+public class SquatStatisticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_squat_statistics);
+    }
+}
