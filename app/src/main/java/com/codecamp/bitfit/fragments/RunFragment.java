656d9468431046efa43c3134fce1e600bed925b2
Vadim <midlogger@gmail.com>
2018-03-01 01:09:34 +0100

    - Object oriented code overhaul (using actual objects of some interfaces instead of implementing everything in the RunFragment class) - Floating button changes color and image on first click (early unfinished stage of a usable switch) - Little changes in the permission handling, the beginning of an overhaul

--- app/src/main/java/com/codecamp/bitfit/fragments/RunFragment.java
+++ app/src/main/java/com/codecamp/bitfit/fragments/RunFragment.java
@@ -3,6 +3,7 @@ package com.codecamp.bitfit.fragments;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -22,7 +23,6 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.codecamp.bitfit.MainActivity;
 import com.codecamp.bitfit.R;
@@ -48,9 +48,7 @@ import static android.content.Context.POWER_SERVICE;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RunFragment extends WorkoutFragment implements OnMapReadyCallback, LocationListener {
-
-    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0;
+public class RunFragment extends WorkoutFragment {
 
     public static RunFragment getInstance() {
         RunFragment fragment = new RunFragment();
@@ -75,38 +73,15 @@ public class RunFragment extends WorkoutFragment implements OnMapReadyCallback, 
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
-        if (ContextCompat.checkSelfPermission(getActivity(),
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(getActivity(),
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-
-            }
-        }
-
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mapFragment.getMapAsync(mapReady);
 
-        FloatingActionButton startStopBut = getView().findViewById(R.id.button_start_stop_run);
+        final FloatingActionButton startStopBut = getView().findViewById(R.id.button_start_stop_run);
         startStopBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startStopBut.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#BB0000")));
+                startStopBut.setImageResource(R.drawable.ic_stop_white_48dp);
             }
         });
     }
@@ -114,73 +89,79 @@ public class RunFragment extends WorkoutFragment implements OnMapReadyCallback, 
     private GoogleMap mMap;
     PolylineOptions lineOptions = new PolylineOptions().color(Color.RED).width(3);
     Polyline line;
-    List<LatLng> points = new ArrayList<LatLng>();
+    List<LatLng> points = new ArrayList<>();
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    OnMapReadyCallback mapReady = new OnMapReadyCallback() {
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            mMap = googleMap;
 
-        User user = DBQueryHelper.findUser();
-        user.getAge();
+            User user = DBQueryHelper.findUser();
+            user.getAge();
 
-        line = mMap.addPolyline(lineOptions);
-        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
-            LocationManager lm = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
-            Criteria criteria = new Criteria();
-            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            line = mMap.addPolyline(lineOptions);
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mMap.setMyLocationEnabled(true);
+                LocationManager lm = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+                Criteria criteria = new Criteria();
+                criteria.setAccuracy(Criteria.ACCURACY_FINE);
 
-            if(!lm.isProviderEnabled(LocationManager.GPS_PROVIDER) && lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
-                // TODO:notify user that GPS should be used for proper precision, network location services aren't suited for this purpose
-            }
-            if(!lm.isProviderEnabled(LocationManager.GPS_PROVIDER) && !lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-                // TODO:notify user that location services are off, pop up a button that allows the user to quickly navigate to the location settings
-            }
-            else{
-                String provider = lm.getBestProvider(criteria,true);
-                Location firstLoc = lm.getLastKnownLocation(provider);
-                if(firstLoc == null){
-                    /*TODO: notify user that there's no location data and that they should wait for a GPS signal*/
+                if(!lm.isProviderEnabled(LocationManager.GPS_PROVIDER) && lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+                    // TODO:notify user that GPS should be used for proper precision, network location services aren't suited for this purpose
+                }
+                if(!lm.isProviderEnabled(LocationManager.GPS_PROVIDER) && !lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+                    // TODO:notify user that location services are off, pop up a button that allows the user to quickly navigate to the location settings
                 }
                 else{
-                    LatLng firstLocLatLng = new LatLng(firstLoc.getLatitude(), firstLoc.getLongitude());
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(firstLocLatLng, 15));
+
 
                     //lm.removeUpdates(this);
                     //wakeLock.release();
+
+                    PowerManager powerManager = (PowerManager) getActivity().getSystemService(POWER_SERVICE);
+                    PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,"RunTracking");
+                    wakeLock.acquire();
+
+                    LocationListener locListener = new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            if(location != null && location.getAccuracy() <= 25){
+                                LatLng curPos = new LatLng(location.getLatitude(),location.getLongitude());
+                                mMap.moveCamera(CameraUpdateFactory.newLatLng(curPos));
+                                points.add(curPos);
+                                line.setPoints(points);
+                                if(previousLoc != null){
+                                    runningDistance += location.distanceTo(previousLoc);
+                                }
+                                else{
+                                    LatLng firstLocLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(firstLocLatLng, 15));
+                                }
+                                previousLoc = location;
+                            }
+                        }
+
+                        @Override
+                        public void onStatusChanged(String s, int i, Bundle bundle) {}
+                        @Override
+                        public void onProviderEnabled(String s) {}
+                        @Override
+                        public void onProviderDisabled(String s) {}
+                    };
+
+                    lm.requestLocationUpdates(1000, 25, criteria, locListener, null);
                 }
-                PowerManager powerManager = (PowerManager) getActivity().getSystemService(POWER_SERVICE);
-                PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,"RunTracking");
-                wakeLock.acquire();
-                lm.requestLocationUpdates(1000, 25, criteria, this, null);
+            }
+            else{
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+                //TODO start the map after getting the permission
             }
         }
-    }
+    };
 
     float runningDistance = 0;
-    Location previousPoint = null;
-
-    @Override
-    public void onLocationChanged(Location location) {
-        if(location != null && location.getAccuracy() <= 25){
-            LatLng curPos = new LatLng(location.getLatitude(),location.getLongitude());
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(curPos));
-            points.add(curPos);
-            line.setPoints(points);
-            if(previousPoint != null){
-                runningDistance += location.distanceTo(previousPoint);
-            }
-        }
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {}
-
-    @Override
-    public void onProviderEnabled(String s) {}
-
-    @Override
-    public void onProviderDisabled(String s) {}
+    Location previousLoc = null;
 
     public Bitmap viewToBitmap(View view) {
         Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
