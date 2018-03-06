e7a09996e0c4efe4109418ba7358845ad75abe41
Vadim <midlogger@gmail.com>
2018-03-06 02:10:04 +0100

    Killed all warnings and implemented sharing here (tell me if it's well placed here, or if there's a better place for it, maybe util)

--- app/src/main/java/com/codecamp/bitfit/fragments/WorkoutFragment.java
+++ app/src/main/java/com/codecamp/bitfit/fragments/WorkoutFragment.java
@@ -1,6 +1,8 @@
 package com.codecamp.bitfit.fragments;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,17 +12,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.codecamp.bitfit.R;
+import com.codecamp.bitfit.util.Util;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
 import com.facebook.share.widget.ShareDialog;
-
-/**
- * Created by MaxBreida on 14.02.18.
- */
 
 public class WorkoutFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
         return super.onCreateView(inflater, container, savedInstanceState);
     }
@@ -29,5 +30,21 @@ public class WorkoutFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.action_bar_items, menu);
         super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    public void shareFragmentViewOnClick(View fragmentView) {
+        // TODO: please tell me if it's okay to place this here, if not it should be easy to move it somewhere else
+
+        Bitmap bitmap = Util.viewToBitmap(fragmentView);
+
+        SharePhoto photo = new SharePhoto.Builder()
+                .setBitmap(bitmap)
+                .build();
+        SharePhotoContent content = new SharePhotoContent.Builder()
+                .addPhoto(photo)
+                .build();
+
+        if(getActivity() != null)
+            ShareDialog.show(getActivity(), content);
     }
 }
