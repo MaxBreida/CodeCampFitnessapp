e6f7c90627f7551737e0aec189d3a1e6289be334
Maximilian Breida <maxbreida@gmail.com>
2018-03-11 14:52:29 +0100

    add easy sharing intent, also use dexter for permission handling of external storage

--- app/src/main/java/com/codecamp/bitfit/fragments/WorkoutFragment.java
+++ app/src/main/java/com/codecamp/bitfit/fragments/WorkoutFragment.java
@@ -1,15 +1,29 @@
 package com.codecamp.bitfit.fragments;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.codecamp.bitfit.MainActivity;
 import com.codecamp.bitfit.OnDialogInteractionListener;
@@ -20,11 +34,27 @@ import com.codecamp.bitfit.util.Util;
 import com.facebook.share.model.SharePhoto;
 import com.facebook.share.model.SharePhotoContent;
 import com.facebook.share.widget.ShareDialog;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class WorkoutFragment extends Fragment {
 
+    private static final int REQUEST_SHARE_ACTION = 7162;
+
+    private String filePath;
     // callbacks to activity
     WorkoutFragment.OnWorkoutInProgressListener callback;
+
 
     @Nullable
     @Override
@@ -42,22 +72,108 @@ public class WorkoutFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
     }
 
-    public void shareFragmentViewOnClick(View fragmentView) {
-        // TODO: please tell me if it's okay to place this here, if not it should be easy to move it somewhere else
+    public void shareFragmentViewOnClick(final View fragmentView) {
+        // Permission handling
+        Dexter.withActivity(getActivity())
+                .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        // permission was granted so now we can do the sharing
+                        Bitmap bitmap = Util.viewToBitmap(fragmentView);
+                        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes);
 
-        Bitmap bitmap = Util.viewToBitmap(fragmentView);
+                        filePath = MediaStore.Images.Media.insertImage(getActivity().getContentResolver(),
+                                bitmap, "Title", null);
 
-        SharePhoto photo = new SharePhoto.Builder()
-                .setBitmap(bitmap)
-                .build();
-        SharePhotoContent content = new SharePhotoContent.Builder()
-                .addPhoto(photo)
-                .build();
+                        if(filePath != null && !filePath.equals("")) {
+                            Intent shareIntent = new Intent(Intent.ACTION_SEND);
 
-        if(getActivity() != null)
-            ShareDialog.show(getActivity(), content);
+                            Uri uri = Uri.parse(filePath);
+
+                            shareIntent.setData(uri);
+                            shareIntent.setType("image/png");
+                            shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+                            shareIntent.putExtra(Intent.EXTRA_TEXT, "Sieh dir meinen letzten Workout an!");
+
+                            // start share activity
+                            if(getActivity() != null)
+                                startActivityForResult(Intent.createChooser(shareIntent, "Teile deinen Workout via..."), REQUEST_SHARE_ACTION);
+
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        if (response.isPermanentlyDenied()) {
+                            // navigate user to app settings
+                            showSettingsDialog();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        // continue to ask user for permission
+                        token.continuePermissionRequest();
+                    }
+                }).check();
     }
 
+    /**
+     * Showing Alert Dialog with Settings option
+     * Navigates user to app settings
+     */
+    private void showSettingsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle(R.string.need_permission);
+        builder.setMessage(R.string.need_permission_storage_message);
+        builder.setPositiveButton(R.string.settings, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                openSettings();
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+
+    }
+
+    // navigating user to app settings
+    private void openSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getActivity().getPackageName(), null);
+        intent.setData(uri);
+        startActivityForResult(intent, 101);
+    }
+
+    /**
+     * After sharing our image, we want to delete the created file so the user has no unnecessary
+     * photos on his device. So we wait for activity result and delete the temp file.
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_SHARE_ACTION) {
+            // delete temp file
+            ContentResolver contentResolver = getActivity().getContentResolver();
+            contentResolver.delete(Uri.parse(filePath), null, null);
+        }
+    }
+
+    /**
+     * tells the mainactivity if a workout is in progress or not
+     */
     public interface OnWorkoutInProgressListener {
         void workoutInProgress(boolean inProgress);
     }
