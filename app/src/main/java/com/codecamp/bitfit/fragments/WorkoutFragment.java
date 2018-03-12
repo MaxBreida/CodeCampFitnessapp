25a578e44a34f2a6441b028e5b7d7512db679f73
Vadim <midlogger@gmail.com>
2018-03-12 01:02:11 +0100

    removed imports and variables that never get used ... since we only need the location permission for this app, we can make the Dexter lib an exclusive import in the run fragment, right?

--- app/src/main/java/com/codecamp/bitfit/fragments/WorkoutFragment.java
+++ app/src/main/java/com/codecamp/bitfit/fragments/WorkoutFragment.java
@@ -1,59 +1,21 @@
 package com.codecamp.bitfit.fragments;
 
-import android.Manifest;
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.media.MediaScannerConnection;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.FileProvider;
-import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.codecamp.bitfit.MainActivity;
-import com.codecamp.bitfit.OnDialogInteractionListener;
 import com.codecamp.bitfit.R;
-import com.codecamp.bitfit.database.User;
-import com.codecamp.bitfit.database.Workout;
-import com.codecamp.bitfit.util.Util;
-import com.facebook.share.model.SharePhoto;
-import com.facebook.share.model.SharePhotoContent;
-import com.facebook.share.widget.ShareDialog;
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.single.PermissionListener;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 
 public class WorkoutFragment extends Fragment {
 
-    private static final int REQUEST_SHARE_ACTION = 7162;
-
-    private String filePath;
     // callbacks to activity
     WorkoutFragment.OnWorkoutInProgressListener callback;
 
